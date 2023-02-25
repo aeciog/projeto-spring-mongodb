885b371142b38e8bef0ebd2fd7a906a0fffa0aac
Aécio Guterres <aecioguterres@gmail.com>
2023-02-25 18:00:23 -0300

    Primeiro commit

---
+++ src/main/java/com/aetios/projetospringmongo/ProjetoSpringMongoApplication.java
@@ -0,0 +1,13 @@
+package com.aetios.projetospringmongo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoSpringMongoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoSpringMongoApplication.class, args);
+	}
+
+}
