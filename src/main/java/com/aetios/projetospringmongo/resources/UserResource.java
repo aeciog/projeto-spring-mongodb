97df2fcbe0e8ae72c7ed2f5f9591bd58d3bf8c69
Aécio Guterres <aecioguterres@gmail.com>
2023-03-08 10:14:32 -0300

    Método uptade inserido

--- src/main/java/com/aetios/projetospringmongo/resources/UserResource.java
+++ src/main/java/com/aetios/projetospringmongo/resources/UserResource.java
@@ -46,6 +46,14 @@ public class UserResource {
         return ResponseEntity.noContent().build();
     }
 
+    @RequestMapping(value="/{id}", method= RequestMethod.PUT) // OU @PutMapping
+    public ResponseEntity<Void> update(@RequestBody UserDTO objDto, @PathVariable String id){
+        User obj = service.fromDTO(objDto); // Conversão DTO para User
+        obj.setId(id); // garatir o id da requisição
+        obj = service.update(obj);
+        return ResponseEntity.noContent().build();
+    }
+
 }
 
 
