5689ba1724de30bb0b6085779481550ff2f8e044
Aécio Guterres <aecioguterres@gmail.com>
2023-03-08 17:13:32 -0300

    Criacao da entidade Post com User

--- src/main/java/com/aetios/projetospringmongo/config/Instantiation.java
+++ src/main/java/com/aetios/projetospringmongo/config/Instantiation.java
@@ -1,26 +1,42 @@
 package com.aetios.projetospringmongo.config;
 
+import com.aetios.projetospringmongo.domain.Post;
 import com.aetios.projetospringmongo.domain.User;
+import com.aetios.projetospringmongo.repository.PostRepository;
 import com.aetios.projetospringmongo.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Configuration;
 
+import java.text.SimpleDateFormat;
 import java.util.Arrays;
+import java.util.TimeZone;
 
 @Configuration
 public class Instantiation implements CommandLineRunner {
     @Autowired
     private UserRepository userRepository;
 
+    @Autowired
+    private PostRepository postRepository;
+
     @Override
     public void run(String... args) throws Exception {
+        SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+        sdf.setTimeZone(TimeZone.getTimeZone("GMT"));
+
         userRepository.deleteAll();
+        postRepository.deleteAll();
 
         User user1 = new User(null, "Usuario 1", "usuario_1@gmail.com");
         User user2 = new User(null, "Usuario 2", "usuario_2@gmail.com");
         User user3 = new User(null, "Usuario 3", "usuario_3@gmail.com");
 
+        Post post1 = new Post(null, sdf.parse("08/03/2023"), "Titulo 1", "Texto a ser inserido.", user1);
+        Post post2 = new Post(null, sdf.parse("08/03/2023"), "Titulo 2", "Texto a ser inserido.", user1);
+        Post post3 = new Post(null, sdf.parse("10/03/2023"), "Titulo 3", "Texto a ser inserido.", user2);
+
         userRepository.saveAll(Arrays.asList(user1, user2, user3));
+        postRepository.saveAll(Arrays.asList(post1, post2, post3));
     }
 }
