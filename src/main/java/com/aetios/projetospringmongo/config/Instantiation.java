e7d03f1575cb89397bb2698ab46664f75f3a4ae6
Aécio Guterres <aecioguterres@gmail.com>
2023-03-09 11:34:36 -0300

    Criação referencia dos posts para usuário

--- src/main/java/com/aetios/projetospringmongo/config/Instantiation.java
+++ src/main/java/com/aetios/projetospringmongo/config/Instantiation.java
@@ -2,6 +2,7 @@ package com.aetios.projetospringmongo.config;
 
 import com.aetios.projetospringmongo.domain.Post;
 import com.aetios.projetospringmongo.domain.User;
+import com.aetios.projetospringmongo.dto.AuthorDTO;
 import com.aetios.projetospringmongo.repository.PostRepository;
 import com.aetios.projetospringmongo.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -31,12 +32,15 @@ public class Instantiation implements CommandLineRunner {
         User user1 = new User(null, "Usuario 1", "usuario_1@gmail.com");
         User user2 = new User(null, "Usuario 2", "usuario_2@gmail.com");
         User user3 = new User(null, "Usuario 3", "usuario_3@gmail.com");
-
-        Post post1 = new Post(null, sdf.parse("08/03/2023"), "Titulo 1", "Texto a ser inserido.", user1);
-        Post post2 = new Post(null, sdf.parse("08/03/2023"), "Titulo 2", "Texto a ser inserido.", user1);
-        Post post3 = new Post(null, sdf.parse("10/03/2023"), "Titulo 3", "Texto a ser inserido.", user2);
-
         userRepository.saveAll(Arrays.asList(user1, user2, user3));
+
+        Post post1 = new Post(null, sdf.parse("08/03/2023"), "Titulo 1", "Texto a ser inserido.", new AuthorDTO(user1));
+        Post post2 = new Post(null, sdf.parse("08/03/2023"), "Titulo 2", "Texto a ser inserido.", new AuthorDTO(user1));
+        Post post3 = new Post(null, sdf.parse("10/03/2023"), "Titulo 3", "Texto a ser inserido.", new AuthorDTO(user2));
         postRepository.saveAll(Arrays.asList(post1, post2, post3));
+
+        user1.getPosts().addAll(Arrays.asList(post1, post2));
+        userRepository.save(user1);
+
     }
 }
--- src/main/java/com/aetios/projetospringmongo/domain/Post.java
+++ src/main/java/com/aetios/projetospringmongo/domain/Post.java
@@ -1,5 +1,6 @@
 package com.aetios.projetospringmongo.domain;
 
+import com.aetios.projetospringmongo.dto.AuthorDTO;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
@@ -18,11 +19,11 @@ public class Post implements Serializable{
     private Date date;
     private String title;
     private String bodyText;
-    private User author;
+    private AuthorDTO author;
 
     public Post(){}
 
-    public Post(String id, Date date, String title, String bodyText, User author) {
+    public Post(String id, Date date, String title, String bodyText, AuthorDTO author) {
         this.id = id;
         this.date = date;
         this.title = title;
@@ -62,11 +63,11 @@ public class Post implements Serializable{
         this.bodyText = bodyText;
     }
 
-    public User getAuthor() {
+    public AuthorDTO getAuthor() {
         return author;
     }
 
-    public void setAuthor(User author) {
+    public void setAuthor(AuthorDTO author) {
         this.author = author;
     }
 
