986f0e8b733f0371212ee1947d549259a9168a18
Aécio Guterres <aecioguterres@gmail.com>
2023-03-10 11:28:20 -0300

    Consulta simples com @Query

--- src/main/java/com/aetios/projetospringmongo/repository/PostRepository.java
+++ src/main/java/com/aetios/projetospringmongo/repository/PostRepository.java
@@ -2,6 +2,7 @@ package com.aetios.projetospringmongo.repository;
 
 import com.aetios.projetospringmongo.domain.Post;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -10,5 +11,7 @@ import java.util.List;
 public interface PostRepository extends MongoRepository<Post, String> {
 
     // Query method
+    @Query("{ 'title': { $regex: ?0, %options: 'i' } }")
+    List<Post> searchTitle(String text);
     List<Post> findByTitleContainingIgnoreCase(String text);
 }
